1393f334adeb3bdd3ce64a41fdfd36a6922a15b0
xzhang774 <xb_zhang@gatech.edu>
2019-04-08 17:14:20 -0400

    Model javadoc added

--- app/src/main/java/com/example/spacetrader/model/ResourceInteractor.java
+++ app/src/main/java/com/example/spacetrader/model/ResourceInteractor.java
@@ -4,12 +4,28 @@ import com.example.spacetrader.entities.Resource;
 import java.util.List;
 
 public class ResourceInteractor extends Interactor {
+
+    /**
+     * Constructor for resource interactor
+     * @param repo the repository
+     */
     public ResourceInteractor(Repository repo) {
         super(repo);
     }
 
-    public List<Resource> getAllResource() { return getRepository().getAllResources(); }
+    /**
+     * Get all resources
+     * @return the list of resources in repository
+     */
+    public List<Resource> getAllResource() {
+        return getRepository().getAllResources();
+    }
 
+    /**
+     * Get resource of certain name
+     * @param name the player name
+     * @return the resource
+     */
     public Resource getResource(String name) {
         List<Resource> resources = getAllResource();
         for (Resource r : resources)
--- app/src/main/java/com/example/spacetrader/view/MainActivity.java
+++ app/src/main/java/com/example/spacetrader/view/MainActivity.java
@@ -88,7 +88,6 @@ public class MainActivity extends AppCompatActivity implements ValueEventListene
         difficultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         difficultySpinner.setAdapter(difficultyAdapter);
 
-        /** Set the numberPicker for skillPoints */
         pPoint = findViewById(R.id.pilot_point);
         fPoint = findViewById(R.id.fighter_point);
         tPoint = findViewById(R.id.trader_point);
